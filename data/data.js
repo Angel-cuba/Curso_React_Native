589b8379ee515fb7d37ec04ea18d7b0c5481627f
Mac_Developer <araozangel842@gmail.com>
2023-09-13 02:26:49 +0300

    Add fake data files.

---
+++ data/data.js
@@ -0,0 +1,86 @@
+
+export const newsData = [
+  {
+    id: 1,
+    title: 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    image: 'https://res.cloudinary.com/dqaerysgb/image/upload/v1650308633/img-3_osvtro.jpg',
+    date: '2 days ago',
+    author: 'James Bond',
+    avatar: 'https://res.cloudinary.com/dqaerysgb/image/upload/v1628020662/samples/imagecon-group.jpg',
+    content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident.----, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+  },
+  {
+    id: 2,
+    title: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+    image:
+      'https://res.cloudinary.com/dqaerysgb/image/upload/v1647972916/can-tinder-make-you-hinder-dating-apps-can-take-a-toll-on-your-mental-health_blhzxx.jpg',
+    date: '2 days ago',
+    author: 'John Doe',
+    avatar: 'https://res.cloudinary.com/dqaerysgb/image/upload/v1628020663/samples/cloudinary-group.jpg',
+    content: 'Lorem ipsum dolor sit amet ',
+  },
+  {
+    id: 3,
+    title: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+    image: 'https://res.cloudinary.com/dqaerysgb/image/upload/v1635399087/Cuban-Food-1_mydd1f.jpg',
+    date: '2 days ago',
+    author: 'Daniel Smith',
+    avatar: 'https://res.cloudinary.com/dqaerysgb/image/upload/v1628020658/samples/bike.jpg',
+    content: 'Lorem ipsum dolor sit amet ',
+  },
+  {
+    id: 4,
+    title: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+    image: 'https://res.cloudinary.com/dqaerysgb/image/upload/v1635399413/picWmxBlt_fx19jk.jpg',
+    date: '2 days ago',
+    author: 'Jhon Travolta',
+    avatar: 'https://res.cloudinary.com/dqaerysgb/image/upload/v1628020656/samples/sheep.jpg',
+    content: 'Lorem ipsum dolor sit amet ',
+  },
+  {
+    id: 5,
+    title: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+    image: 'https://res.cloudinary.com/dqaerysgb/image/upload/v1635360680/mexico_h9dcjv.jpg',
+    date: '2 days ago',
+    author: 'Lory Jhonson',
+    avatar: 'https://res.cloudinary.com/dqaerysgb/image/upload/v1628020661/samples/ecommerce/car-interior-design.jpg',
+    content: 'Lorem ipsum dolor sit amet ',
+  },
+  {
+    id: 6,
+    title: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+    image: 'https://res.cloudinary.com/dqaerysgb/image/upload/v1635360679/italian-food_jsa7c0.jpg',
+    date: '2 days ago',
+    author: 'Vera Wang',
+    avatar: 'https://res.cloudinary.com/dqaerysgb/image/upload/v1628020662/samples/ecommerce/accessories-bag.jpg',
+    content: 'Lorem ipsum dolor sit amet ',
+  },
+];
+
+export const exploreData = [
+  {
+    id: 1,
+    title: 'Technology',
+    image: 'https://res.cloudinary.com/dqaerysgb/image/upload/v1671549590/New_Feed_React-Native/technology_qaobnr.jpg',
+  },
+  {
+    id: 2,
+    title: 'Sports',
+    image: 'https://res.cloudinary.com/dqaerysgb/image/upload/v1671549586/New_Feed_React-Native/sports_h9ww2l.jpg',
+  },
+  {
+    id: 3,
+    title: 'Food',
+    image: 'https://res.cloudinary.com/dqaerysgb/image/upload/v1671549637/New_Feed_React-Native/food_ntuami.jpg',
+  },
+  {
+    id: 4,
+    title: 'Science',
+    image: 'https://res.cloudinary.com/dqaerysgb/image/upload/v1671549583/New_Feed_React-Native/science_kckiki.jpg',
+  },
+  {
+    id: 5,
+    title: 'Lifestyle',
+    image: 'https://res.cloudinary.com/dqaerysgb/image/upload/v1671549578/New_Feed_React-Native/lifestyle-1_c1c7xz.jpg',
+  },
+]
