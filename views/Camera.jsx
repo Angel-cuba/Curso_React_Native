184d485112a6da8e26eb094b4013bc466f2b0612
Mac_Developer <araozangel842@gmail.com>
2023-09-19 14:02:32 +0300

    Implementing the camera view
    The user can take a picture and see it in the list view.

--- views/Camera.jsx
+++ views/Camera.jsx
@@ -1,16 +1,22 @@
-import { View, TouchableOpacity, StyleSheet, Alert } from 'react-native';
+import { View, TouchableOpacity, StyleSheet, Alert, FlatList, Image } from 'react-native';
 import React from 'react';
 import GlobalView from '../component/global/GlobalView';
 
 import { Camera, CameraType } from 'expo-camera';
 import StyledText from '../component/Text/StyledText';
 import ImageViewer from '../component/ImageViewer';
+import CustomButton from '../component/CustomButton';
+
+import * as Location from 'expo-location';
 
 const CameraPicker = () => {
   const [type, setType] = React.useState(CameraType.back);
   const [camera, setCamera] = React.useState(null); // [1
   const [hasPermission, setHasPermission] = React.useState(null);
   const [image, setImage] = React.useState(null);
+  const imageRef = React.useRef([]);
+
+  const [location, setLocation] = React.useState(null);
 
   React.useEffect(() => {
     (async () => {
@@ -25,9 +31,23 @@ const CameraPicker = () => {
     Alert.alert('Permisos denegados');
     return <View />;
   }
+
+const getLocation = async () => {
+  let { status } = await Location.requestForegroundPermissionsAsync();
+  if (status !== 'granted') {
+    Alert.alert('Permisos denegados');
+    return;
+  }
+  let location = await Location.getCurrentPositionAsync({});
+  setLocation(location);
+}
+
   async function takePicture() {
+    await getLocation();
+    setImage(null);
     if (camera) {
       const photo = await camera.takePictureAsync();
+      imageRef.current.push(photo.uri);
       setImage(photo.uri);
     }
   }
@@ -35,28 +55,67 @@ const CameraPicker = () => {
   function toggleType() {
     setType((current) => (current === CameraType.back ? CameraType.front : CameraType.back));
   }
+
+  const deleteImgs = () => {
+    imageRef.current = [];
+    setImage(null);
+  };
+
   return (
     <GlobalView>
-      <Camera
-        style={{ flex: 1, width: '100%', alignSelf: 'center', borderRadius: 10 }}
-        type={type}
-        ref={(ref) => {
-          setCamera(ref);
-        }}
+      {!image ? (
+        <Camera
+          style={{ width: '96%', height: 450, alignSelf: 'center', borderRadius: 10 }}
+          type={type}
+          ref={(ref) => {
+            setCamera(ref);
+          }}
+        />
+      ) : (
+        <ImageViewer
+          selectedImage={image}
+          placeHolderImage={
+            'https://res.cloudinary.com/dqaerysgb/image/upload/v1648218398/istockphoto-1132926013-612x612_t1xwec.jpg'
+          }
+        />
+      )}
+      {imageRef.current.length > 2 ? (
+        <CustomButton label="Borrar imagenes" onPress={deleteImgs} color="#ff0000c2" />
+      ) : (
+        <View style={styles.container}>
+          <TouchableOpacity style={styles.button} onPress={toggleType}>
+            <StyledText big bold style={styles.text}>
+              Flip
+            </StyledText>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.button} onPress={takePicture}>
+            <StyledText big bold style={styles.text}>
+              Take picture
+            </StyledText>
+          </TouchableOpacity>
+        </View>
+      )}
+      <ImageViewer
+        selectedImage={image}
+        placeHolderImage={
+          'https://res.cloudinary.com/dqaerysgb/image/upload/v1648218398/istockphoto-1132926013-612x612_t1xwec.jpg'
+        }
       />
-      <View style={styles.container}>
-        <TouchableOpacity style={styles.button} onPress={toggleType}>
-          <StyledText big bold style={styles.text}>
-            Flip
-          </StyledText>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.button} onPress={takePicture}>
-          <StyledText big bold style={styles.text}>
-            Take picture
-          </StyledText>
-        </TouchableOpacity>
-      </View>
-      <ImageViewer selectedImage={image} placeHolderImage={'https://res.cloudinary.com/dqaerysgb/image/upload/v1648218398/istockphoto-1132926013-612x612_t1xwec.jpg'}/>
+      {
+        !location ? <StyledText small>Waiting.....</StyledText> : <StyledText small bold>{
+          `Latitud: ${location.coords.latitude} \n`
+          + `Longitud: ${location.coords.longitude} \n`
+          + `Altitud: ${location.coords.altitude} \n`
+          
+          }</StyledText>
+      }
+        <FlatList
+          data={imageRef.current}
+          renderItem={(item) => <CurrentItem item={item.item} />}
+          keyExtractor={(item) => item.id}
+          numColumns={3}
+          showsVerticalScrollIndicator={false}
+        />
     </GlobalView>
   );
 };
@@ -77,3 +136,18 @@ const styles = StyleSheet.create({
     textAlign: 'center',
   },
 });
+
+const CurrentItem = ({ item }) => {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: 'space-around',
+        paddingHorizontal: 10,
+        paddingVertical: 10,
+      }}
+    >
+      <Image source={{ uri: item }} style={{ width: 100, height: 100, borderRadius: 5 }} />
+    </View>
+  );
+};
