1ed31ea5af74bb850d7aee5a66823b2ffea1c715
Mac_Developer <araozangel842@gmail.com>
2023-09-19 13:58:17 +0300

    Summary:
    - The user can choose to upload a picture from the gallery. Also can choose more than one picture, at least 3 pictures.
    - After to reach the minimum of pictures, the button to upload will automatically disappear and appear the button to delete the selected pictures.

--- views/Profile.jsx
+++ views/Profile.jsx
@@ -1,20 +1,21 @@
-import { Text, TouchableOpacity, Alert } from 'react-native';
+import { Text, TouchableOpacity, Alert, Image, View, FlatList, ScrollView } from 'react-native';
 import React from 'react';
 import GlobalView from '../component/global/GlobalView';
 import StyledText from '../component/Text/StyledText';
 import { Context } from '../navigator/Navigator';
 import * as ImagePicker from 'expo-image-picker';
-import Button from '../component/Button';
 import ImageViewer from '../component/ImageViewer';
 import InputComponet from '../component/InputComponent';
+import CustomButton from '../component/CustomButton';
 
 const Profile = () => {
   const { setUser } = React.useContext(Context);
   const [image, setImage] = React.useState(null);
+  const imageRef = React.useRef([]);
   const [formulario, setFormulario] = React.useState({
     nombre: '',
     apellidos: '',
-    imagen: '',
+    imagenes: imageRef.current,
   });
 
   const pickImage = async () => {
@@ -30,8 +31,9 @@ const Profile = () => {
       quality: 1, // Calidad de la imagen
     });
     if (!result.canceled) {
-      setImage(result.assets[0].uri);
-      setFormulario({ ...formulario, imagen: result.assets[0].uri });
+      const { uri } = result.assets[0];
+      imageRef.current.push(uri);
+      setImage(uri);
     }
   };
   const changeText = (event) => {
@@ -41,53 +43,85 @@ const Profile = () => {
     setFormulario({ ...formulario, apellidos: event });
   };
 
+  const deleteImgs = () => {
+    imageRef.current = [];
+    setImage(null);
+  };
+
   return (
     <GlobalView>
-      <StyledText big bold left>
-        Perfil
-      </StyledText>
-      <Text
-        style={{
-          fontSize: 20,
-          textAlign: 'center',
-          marginVertical: 20,
-          marginHorizontal: 20,
-        }}
-      >
-        Form
-      </Text>
-      <InputComponet label="Nombre" onChangeText={changeText} />
-      <InputComponet label="Apellidos" onChangeText={changeTextApellidos} />
-      <ImageViewer
-        selectedImage={image}
-        placeHolderImage={
-          'https://res.cloudinary.com/dqaerysgb/image/upload/v1648218398/istockphoto-1132926013-612x612_t1xwec.jpg'
-        }
-      />
-      <Button label="Seleccionar foto" onPress={pickImage} />
-      <Button label="Guardar" onPress={() => console.log('Formulario', formulario)} />
-      <TouchableOpacity
-        style={{
-          backgroundColor: 'blue',
-          padding: 10,
-          borderRadius: 10,
-          marginTop: 20,
-          marginHorizontal: 30,
-        }}
-        onPress={() => setUser('')}
-      >
-        <Text
+      <ScrollView>
+        <StyledText big bold left>
+          Perfil
+        </StyledText>
+        <InputComponet label="Nombre" onChangeText={changeText} />
+        <InputComponet label="Apellidos" onChangeText={changeTextApellidos} />
+        <ImageViewer
+          selectedImage={image}
+          placeHolderImage={
+            'https://res.cloudinary.com/dqaerysgb/image/upload/v1648218398/istockphoto-1132926013-612x612_t1xwec.jpg'
+          }
+        />
+        {imageRef.current.length < 3 ? (
+          <View>
+            <CustomButton label="Seleccionar foto" onPress={pickImage} />
+          </View>
+        ) : (
+          <View>
+            <StyledText small bold style={{ color: 'red' }}>
+              Limite de fotos alcanzado
+            </StyledText>
+            <CustomButton label="Delete" onPress={deleteImgs} color="#ff0000" />
+          </View>
+        )}
+        <CustomButton label="Guardar" onPress={() => console.log('Formulario', formulario)} />
+        <TouchableOpacity
           style={{
-            color: 'white',
-            textAlign: 'center',
-            fontWeight: 'bold',
+            backgroundColor: 'blue',
+            padding: 10,
+            borderRadius: 10,
+            marginTop: 20,
+            marginHorizontal: 30,
           }}
+          onPress={() => setUser('')}
         >
-          Cerrar sesión
-        </Text>
-      </TouchableOpacity>
+          <Text
+            style={{
+              color: 'white',
+              textAlign: 'center',
+              fontWeight: 'bold',
+            }}
+          >
+            Cerrar sesión
+          </Text>
+        </TouchableOpacity>
+      </ScrollView>
+      <View>
+        <FlatList
+          data={imageRef.current}
+          renderItem={(item) => <CurrentItem item={item.item} />}
+          keyExtractor={(index) => index.toString()}
+          showsVerticalScrollIndicator={false}
+          numColumns={3}
+        />
+      </View>
     </GlobalView>
   );
 };
 
 export default Profile;
+
+const CurrentItem = ({ item }) => {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: 'space-around',
+        paddingHorizontal: 10,
+        paddingVertical: 10,
+      }}
+    >
+      <Image source={{ uri: item }} style={{ width: 100, height: 100, borderRadius: 5 }} />
+    </View>
+  );
+};
